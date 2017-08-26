a43ac5e4f1af47474f6cb2ad0f73adc5b5bfddc4
kurbakovDH <dmytro.kurbakov@deliveryhero.com>
2017-08-26 19:19:50 +0200

    some HTable function

--- HashTable/HTable.cpp
+++ HashTable/HTable.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 
 namespace HashTable{
 template<typename T>
@@ -15,7 +16,7 @@ struct Node{
 template <class T>
 class HTable{
 private:
-    uint64_t Buffer;
+    uint64_t BUFFER;
     uint64_t Size;
     Node<T>* Table;
 
@@ -36,26 +37,72 @@ public:
 
 namespace HashTable{
 template <class T>    
-uint64_t HTable<T>::hash_function(T value){}
+uint64_t HTable<T>::hash_function(T value){
+    return std::hash<T>(value) % BUFFER;
+}
 
 template <class T>
-HTable<T>::HTable(){}
+HTable<T>::HTable(){
+    BUFFER = 100;
+    Size = 0;
+    Table = new Node<T>[BUFFER];
+    for(int i=0; i<BUFFER; i++){
+        Table[i] = nullptr;
+    }
+}
 
 template <class T>
-HTable<T>::~HTable(){}
+HTable<T>::~HTable(){
+    delete Table;
+}
 
 template <class T>
-void HTable<T>::get_size(){}
+void HTable<T>::get_size(){
+    return Size;
+}
 
 template <class T>
-bool HTable<T>::is_empty(){}
+bool HTable<T>::is_empty(){
+    return Size == 0;
+}
 
 template <class T>
-void HTable<T>::insert(T key){}
+void HTable<T>::insert(T key){
+    uint64_t idx = hash_function(key);
+    if(Table[idx] == nullptr){
+        Node<T>* new_node = new Node<T>(key);
+        Table[idx] = new_node;
+        return;
+    }
+
+    Node<T>* cur;
+    while(cur->Next != nullptr){
+        if(cur->data == key) return;
+        cur = cur->Next;
+    }
+    
+    Node<T>* new_node = new Node<T>(key);
+    cur->Next = new_node;
+    return;
+}
 
 template <class T>
-bool HTable<T>::find(T key){}
+bool HTable<T>::find(T key){
+    uint64_t idx = hash_function(key);
+    Node<T>* cur = Table[idx];
+
+    while (cur != nullptr){
+        if(cur->data == key){
+            return true;
+        }
+        else{
+            cur = cur->Next;
+        }
+    }
+    return false;
+}
 
 template <class T>    
 void HTable<T>::remove(T key){}
+
 };
